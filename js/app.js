5ec017476a412995f872e752a67f1cb2abf0988a
Your Name <giovilaza@gmail.com>
2016-02-17 13:27:45 +0100

    load model
    step 4, lab2

--- js/app.js
+++ js/app.js
@@ -10,6 +10,6 @@ $(function() {
 
 
 	//And create the needed controllers and views
-	var exampleView = new ExampleView($("#exampleView"));
+	var exampleView = new ExampleView($("#exampleView"),model);
 
 });
