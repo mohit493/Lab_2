3329503317dc27ba7c19d04af3e9035b1774bd7f
mohit493 <mohityadav0493@gmail.com>
2016-02-23 16:06:28 +0100

    Updated View 1+2

--- js/app.js
+++ js/app.js
@@ -4,14 +4,8 @@ $(function () {
 
 
 
-<<<<<<< HEAD
-=======
-	//And create the needed controllers and views
-	var exampleView = new ExampleView($("#exampleView"),model);
-	var exampleViewController = new ExampleViewController(exampleView,model);
->>>>>>> master
-
-    //And create the needed controllers and views
     var exampleView = new ExampleView($("#exampleView"), model);
+
     var selectDishView = new SelectDishView($("#selectDishView"), model);
+
 });
--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -1,32 +1,20 @@
 //ExampleView Object constructor
-<<<<<<< HEAD
+
 var ExampleView = function (container, model) {
 
     var changeview1 = document.getElementById("createDinner");
+
     changeview1.addEventListener("click", function () {
+
+
         $("#homeView").hide();
         $("#sidebarView").show();
         $("#searchbar").show();
         $("#selectDishView").show();
 
 
-    })
+    });
 
 
 
 }
-=======
-var ExampleView = function (container,model) {
-	
-	// Get all the relevant elements of the view (ones that show data
-  	// and/or ones that responed to interaction)
-	this.numberOfGuests = container.find("#numberOfGuests");
-	this.plusButton = container.find("#plusGuest");
-	this.minusButton = container.find("#minusGuest");
-	
-	this.numberOfGuests.html(model.getNumberOfGuests());
-	model.addObserver(this);
-	
-}
- 
->>>>>>> master
