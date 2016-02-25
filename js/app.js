4e67dcbc61d370e6629ab685071698cf178b994f
Giovanna Vilaza <giovilaza@gmail.com>
2016-02-25 15:08:20 +0100

    changes in model

--- js/app.js
+++ js/app.js
@@ -2,8 +2,6 @@ $(function () {
     //We instantiate our model
     var model = new DinnerModel();
 
-    var dishDescriptionView = new DishDescriptionView($("#dishDescriptionView"), model);
-
-    var sidebarView = new SidebarView($("#sidebarView"), model);
+	var instructionsView = new InstructionsView($("#instructionsView"), model);
 
 });
