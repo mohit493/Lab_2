1c49ad4d9a769dddc32e00b4aa618760976e68fe
mohit493 <mohit.yadav0493@gmail.com>
2016-02-26 03:59:21 +0100

    num of guest controller
    sss

---
+++ js/numGuestsViewController.js
@@ -0,0 +1,20 @@
+//NumGuestViewController Object constructor
+
+var NumGuestsViewController = function (view, model) {
+
+    var backview = document.getElementById("backButton");
+
+    backview.addEventListener("click", function () {
+        $('#summaryView').hide();
+        $('#numGuestsView').hide();
+        $('#instructionsView').hide();
+        $('#selectDishView').show();
+        $('#sidebarView').show();
+        $('#searchbar').show();
+
+
+    });
+
+
+
+}
