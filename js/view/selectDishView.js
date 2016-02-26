fcee99f428694eb9784c401c6f0b09c2cf730f05
mohit493 <mohit.yadav0493@gmail.com>
2016-02-26 05:55:36 +0100

    added Id for click
    A bit of styling issue

--- js/view/selectDishView.js
+++ js/view/selectDishView.js
@@ -13,7 +13,8 @@ var SelectDishView = function (container, model) {
     var sessionlist = "";
 
     for (var x = 0; x < model.getAllDishes("starter").length; x++) {
-        sessionlist += '<div class="col-md-3">' + '<img src="' + 'images/' + model.getAllDishes("starter")[x].image + '" class="img-responsive" alt="' + model.getAllDishes("starter")[x].name + '">' + '<h5>' + model.getAllDishes("starter")[x].name + '</h5>' + '</div>'
+        sessionlist += '<li id=list >' + '<div class="col-md-3">' + '<img src="' + 'images/' + model.getAllDishes("starter")[x].image + '" class="img-responsive" alt="' + model.getAllDishes("starter")[x].name + '">' + '<h5>' + model.getAllDishes("starter")[x].name + '</h5>' + '</div>';
+        sessionlist += '</li>';
 
     }
     document.getElementById("starters").innerHTML = sessionlist + "<br> <br>";
@@ -21,7 +22,9 @@ var SelectDishView = function (container, model) {
 
 
     for (var x = 0; x < model.getAllDishes("main dish").length; x++) {
-        sessionlist += '<div class="col-md-3">' + '<img src="' + 'images/' + model.getAllDishes("main dish")[x].image + '" class="img-responsive" alt="' + model.getAllDishes("main dish")[x].name + '">' + '<h5>' + model.getAllDishes("main dish")[x].name + '</h5>' + '</div>'
+        sessionlist += '<li id=list>' + '<div class="col-md-3">' + '<img src="' + 'images/' + model.getAllDishes("main dish")[x].image + '" class="img-responsive" alt="' + model.getAllDishes("main dish")[x].name + '">' + '<h5>' + model.getAllDishes("main dish")[x].name + '</h5>' + '</div>';
+
+        sessionlist += '</li>';
 
     }
     document.getElementById("main").innerHTML = sessionlist + "<br> <br>";
@@ -30,12 +33,30 @@ var SelectDishView = function (container, model) {
 
 
     for (var x = 0; x < model.getAllDishes("dessert").length; x++) {
-        sessionlist += '<div class="col-md-3">' + '<img src="' + 'images/' + model.getAllDishes("dessert")[x].image + '" class="img-responsive" alt="' + model.getAllDishes("dessert")[x].name + '">' + '<h5>' + model.getAllDishes("dessert")[x].name + '</h5>' + '</div>'
+        sessionlist += '<li id=list ">' + '<div class="col-md-3">' + '<img src="' + 'images/' + model.getAllDishes("dessert")[x].image + '" class="img-responsive" alt="' + model.getAllDishes("dessert")[x].name + '">' + '<h5>' + model.getAllDishes("dessert")[x].name + '</h5>' + '</div>';
+
+        sessionlist += '</li>';
 
     }
     document.getElementById("desserts").innerHTML = sessionlist + "<br> <br>";
     sessionlist = "";
 
+
+
+    // Search functionality in the sidebar
+    var sourceArr = [];
+
+    $("#searchDish").typeahead({
+        local: sourceArr
+    });
+
+
+    $("#searchDish").keyup(function () {
+        var userInput = $(this).val();
+        $("#selectDishView li").map(function (index, value) {
+            $(value).toggle($(value).text().toLowerCase().indexOf(userInput) >= 0);
+        });
+    });
 
 
 
